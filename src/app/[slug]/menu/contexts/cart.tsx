862d110f4d261e6941272695110349538b075585
Walisson Carlos <walissoncsl@icloud.com>
2026-03-14 16:52:40 -0300

    feat: ensure cart summary is shown on restaurant page

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -12,17 +12,18 @@ export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
   total: number;
+  totalQuantity: number;
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
   increaseProductQuantity: (productId: string) => void;
   removeProduct: (productId: string) => void;
-  
 }
 
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   total: 0,
+  totalQuantity: 0,
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
@@ -37,8 +38,10 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
 
   const total = products.reduce((acc, product) => {
     return acc + product.price * product.quantity;
-  }, 0)
-
+  }, 0);
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
   const toggleCart = () => {
     setIsOpen((prev) => !prev);
   };
@@ -61,36 +64,34 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       });
     });
   };
-
   const decreaseProductQuantity = (productId: string) => {
-    setProducts(prevProducts => {
-      return prevProducts.map(prevProduct => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
         if (prevProduct.id !== productId) {
           return prevProduct;
         }
-          if (prevProduct.quantity == 1) {
-            return prevProduct;
-          }
-          return {...prevProduct, quantity: prevProduct.quantity - 1}
+        if (prevProduct.quantity === 1) {
+          return prevProduct;
+        }
+        return { ...prevProduct, quantity: prevProduct.quantity - 1 };
       });
     });
   };
-
   const increaseProductQuantity = (productId: string) => {
     setProducts((prevProducts) => {
       return prevProducts.map((prevProduct) => {
         if (prevProduct.id !== productId) {
-          return prevProduct
-        };
-        return { ...prevProduct, quantity: prevProduct.quantity + 1}
+          return prevProduct;
+        }
+        return { ...prevProduct, quantity: prevProduct.quantity + 1 };
       });
     });
   };
-
   const removeProduct = (productId: string) => {
-    setProducts(prevProducts => prevProducts.filter(prevProduct => prevProduct.id !== productId));
-  }
-
+    setProducts((prevProducts) =>
+      prevProducts.filter((prevProduct) => prevProduct.id !== productId),
+    );
+  };
   return (
     <CartContext.Provider
       value={{
@@ -102,6 +103,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         increaseProductQuantity,
         removeProduct,
         total,
+        totalQuantity,
       }}
     >
       {children}
